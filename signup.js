e79f36d62159f895547cf3fd9a8db127f2784ed8
Muhammed Muhammed Olayinka <106432914+Moh922@users.noreply.github.com>
2022-11-06 01:39:58 +0100

    Add files via upload

---
+++ signup.js
@@ -0,0 +1,63 @@
+/*const register = document.getElementById("myform")
+const submitBtn = document.querySelector('.send')
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  var userName =document.getElementById("userName").value;
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+    
+ fetch("https://url-shortener-bc.herokuapp.com/api/v1/users/signup", {
+    method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            userName: userName,
+            email: email,
+            password:password,
+        }), 
+    })
+     .then((res) => {
+        return res.json();
+    })
+    .then((data) =>{
+      if (data != null ) {
+        window.location.href = '/login.html'
+      }
+      console.log(data);
+    })
+    .catch((err) => console.log(err));
+}
+
+submitBtn.addEventListener('click', (e) => handleSubmit(e))*/
+
+
+const form = document.getElementById("myform");
+    form.onsubmit = function (e) {
+        e.preventDefault();
+        var userName = document.getElementById("userName").value;
+        var email = document.getElementById("email").value;
+        var password = document.getElementById("password").value;
+
+        window.fetch("https://url-shortener-bc.herokuapp.com/api/v1/users/signup", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                userName: userName,
+                email: email,
+                password: password,
+            }),
+        })
+
+            .then(function (res) {
+                return res.json();
+            })
+            .then(function (data) {
+                if (data != 'null') {
+                    window.location.href = './login.html';
+                }
+                console.log(data);
+            })
+            .catch(function (err) {
+                return console.log(err);
+            });
+    }  
